fd0197e3f8e784d2793ee9ccdc4a4f850000b0d8
Kira Mishima <kiramishima@outlook.com>
2024-05-26 20:41:24 -0600

    Fix HW scripts

--- week_2/homework/hpo.py
+++ week_2/homework/hpo.py
@@ -6,7 +6,7 @@ import numpy as np
 from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 from hyperopt.pyll import scope
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("random-forest-hyperopt")
@@ -46,7 +46,7 @@ def run_optimization(data_path: str, num_trials: int):
             print(f"default artifact URI: '{mlflow.get_artifact_uri()}'")
 
             y_pred = rf.predict(X_val)
-            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            rmse = root_mean_squared_error(y_val, y_pred)
             mlflow.log_metric("rmse", rmse)
 
         return {'loss': rmse, 'status': STATUS_OK}
--- week_2/homework/train.py
+++ week_2/homework/train.py
@@ -4,7 +4,7 @@ import click
 import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 
 def load_pickle(filename: str):
@@ -33,7 +33,7 @@ def run_train(data_path: str):
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
 
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = root_mean_squared_error(y_val, y_pred)
         mlflow.log_metric("RMSE", rmse)
 
 
